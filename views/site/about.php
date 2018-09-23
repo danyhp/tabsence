65fd83228a1e8dc8f58ee1e9644721c7fa7b02eb
danyhp <dany.panggabean@gmail.com>
2018-09-23 19:38:15 +0700

    About Changes

--- views/site/about.php
+++ views/site/about.php
@@ -2,16 +2,36 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\Html;
-
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
+<div class="col-lg-8">
+    <div class="site-about">
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+        <p>
+            TABSENCE adalah aplikasi absensi yang
+            mudah digunakan untuk membantu
+            Anda dalam mengelola toko dan
+            memantau karyawan melalui website
+            TABSENCE. Hanya dengan mengakses
+            internet, Anda tetap bisa mengelola
+            karyawan Anda dengan lebih mudah.
+            Perhitungan absensi, uang lembur,
+            penggajian, jatah cut dan libur dapat
+            Anda lakukan dengan mudah melalui
+            aplikasi TABSENCE
+        </p>
 
-    <code><?= __FILE__ ?></code>
+        <p>
+            TABSENCE cocok digunakan untuk segala jenis usaha
+            mulai dari kafe, kedai kopi, barbershop, rumah
+            makan, food truck, toko baju, salon hingga retail.
+            Aplikasi TABSENCE ini juga dapat membantu dan
+            mengelola karyawan tdak hanya satu cabang saja,
+            tetapi juga berbagai cabang usaha yang anda miliki.
+            Silakan pilih skema yang sesuai dengan usaha anda.
+        </p>
+
+        <!--    <code>--><? //= __FILE__ ?><!--</code>-->
+    </div>
 </div>
