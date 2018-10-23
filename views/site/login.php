3d0c3cfafe88621bbbf314c41d44cf628970779e
danyhp <dany.panggabean@gmail.com>
2018-10-23 11:21:40 +0700

    .htaccess pada root tabsence folder

--- views/site/login.php
+++ views/site/login.php
@@ -22,7 +22,8 @@ $fieldOptions2 = [
 
 <div class="login-box">
     <div class="login-logo">
-        <?= Html::a('<span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+        <h3 ><strong>PLEASE LOGIN</strong></H3>
+        <!-- <? //=  Html::a('<span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?> -->
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
@@ -54,17 +55,17 @@ $fieldOptions2 = [
 
         <?php ActiveForm::end(); ?>
 
-<!--        <div class="social-auth-links text-center">-->
-<!--            <p>- OR -</p>-->
-<!--            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in-->
-<!--                using Facebook</a>-->
-<!--            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign-->
-<!--                in using Google+</a>-->
-<!--        </div>-->
+        <!--        <div class="social-auth-links text-center">-->
+        <!--            <p>- OR -</p>-->
+        <!--            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in-->
+        <!--                using Facebook</a>-->
+        <!--            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign-->
+        <!--                in using Google+</a>-->
+        <!--        </div>-->
         <!-- /.social-auth-links -->
 
-<!--        <a href="#">I forgot my password</a><br>-->
-<!--        <a href="register.html" class="text-center">Register a new membership</a>-->
+        <!--        <a href="#">I forgot my password</a><br>-->
+        <!--        <a href="register.html" class="text-center">Register a new membership</a>-->
 
     </div>
     <!-- /.login-box-body -->
